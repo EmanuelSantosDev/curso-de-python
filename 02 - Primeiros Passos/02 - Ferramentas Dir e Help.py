13d151ae02f42a0a8dd7ce240a5aeeaab61c7c14
Emanuel Santos <eu.emanuel.santos@outlook.com>
2022-06-15 21:49:18 -0300

    Update

--- 02 - Primeiros Passos/02 - Ferramentas Dir e Help.py
+++ 02 - Primeiros Passos/02 - Ferramentas Dir e Help.py
@@ -3,18 +3,18 @@
 Ferramenta dir()
 ----------------
 
-  - Apresenta no terminal uma lista de funções e métodos que podem ser
-    utilziados em algum tipo de dado
+    - Apresenta no terminal uma lista de funções e métodos que podem ser
+      utilziados em algum tipo de dado
 
-  - Sintaxe: dir(valor/tipo de dado)
+    - Sintaxe: dir(valor/tipo de dado)
 
-  - Se rodar diretamente no interpretador Python, via terminal, ele vai mostrar
-    todos os pacotes de funções disponíveis para o arquivo:
+    - Se rodar diretamente no interpretador Python, via terminal, ele vai mostrar
+      todos os pacotes de funções disponíveis para o arquivo:
 
-    terminal: python
-    terminal: >>> dir()
-    output: ['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__']
-    terminal: >>> dir(__builtins__)
+      terminal: python
+      terminal: >>> dir()
+      output: ['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__', '__spec__']
+      terminal: >>> dir(__builtins__)
 
 """
 
@@ -29,10 +29,10 @@ print(dir(float))
 Ferramenta help()
 ----------------- 
 
-  - Apresenta as instruções de utilização das funções e métodos disponíveis
-    para determinado tipo de dado 
+    - Apresenta as instruções de utilização das funções e métodos disponíveis
+      para determinado tipo de dado 
 
-  - Sintaxe: help(funcao) ou help(valor/tipo_de_dado.método/função) 
+    - Sintaxe: help(funcao) ou help(valor/tipo_de_dado.método/função) 
   
 """
 
